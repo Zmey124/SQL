6a2c2040d997cae722b599d4f1a5971fe00aca1a
Zmey <zmey12489@yandex.ru>
2025-01-26 13:07:51 +0700

    Доработка

--- SQL.py
+++ SQL.py
@@ -25,14 +25,19 @@ def create_db(conn):
                     """)
         conn.commit()
 
-def add_client(conn,first_name, last_name, email, phones=None):
+def add_client(conn,first_name, last_name, email, number=None):
     with conn.cursor() as cur:
         cur.execute("""
             INSERT INTO clients(first_name,last_name,email)
             VALUES (%s,%s,%s)
             RETURNING first_name,last_name,email;
-            """, (first_name,last_name,email))    
-        print(cur.fetchone())
+            """, (first_name,last_name,email)) 
+        new_man = cur.fetchone()
+        print(f"Добавлен новый клиент: {new_man}")
+        if number:
+            add_phone(conn,new_man[0],number)   
+        return new_man
+        
 
 def add_phone(conn,id, number):
     with conn.cursor() as cur:
@@ -41,13 +46,28 @@ def add_phone(conn,id, number):
             VALUES (%s,%s)
             RETURNING id,number;
             """, (id,number))
-        print(cur.fetchone())
+        phone_name = cur.fetchone()
+        print(f"Добавлен телефон: {phone_name}")
 
 def change_client(conn,id, first_name=None, last_name=None, email=None, number=None):
     with conn.cursor() as cur:
-        cur.execute("""
-            UPDATE clients SET first_name=%s,last_name=%s,email=%s WHERE id=%s;
-            """, (first_name,last_name,email,id))
+        list_name = []
+        paramms = []
+        if first_name is not  None:
+            list_name.append('first_name = %s')
+            paramms.append(first_name)
+        if last_name is not None:
+            list_name.append('last_name = %s')
+            paramms.append(last_name)
+        if email is not None:
+            list_name.append('email = %s')
+            paramms.append(email)
+        if number is not None:
+            list_name.append('number = %s')
+            paramms.append(number)
+        paramms.append(id)
+        value1 = f"UPDATE clients SET {','.join(list_name)} WHERE id = %s"
+        cur.execute(value1,paramms)
         cur.execute("""
             SELECT * FROM clients;
         """)
@@ -77,21 +97,21 @@ def delete_client(conn,id):
 def find_client(conn,first_name=None, last_name=None, email=None, number=None):
     with conn.cursor() as cur:
         cur.execute("""
-            SELECT c.first_name,c.last_name,c.email,p.number FROM clients c
+            SELECT * FROM clients c
             LEFT JOIN phone p ON c.id = p.id
-            WHERE c.first_name=%s OR c.last_name=%s OR c.email=%s OR p.number=%s;
-            """, (first_name,last_name,email,number,)
-        )
-        print(cur.fetchone())
+            WHERE (first_name = %(first_name)s OR %(first_name)s IS NULL)
+            AND (last_name = %(last_name)s OR %(last_name)s IS NULL)
+            AND (email = %(email)s OR %(email)s IS NULL)
+            OR (number = %(number)s OR %(number)s IS NULL);
+            """, {'first_name': first_name, 'last_name': last_name, 'email': email, 'number': number})
+        return cur.fetchall()
 
-
-with psycopg2.connect(database="test_netology", user="postgres", password="1234") as conn:
-    create_db(conn)
-    add_client(conn,'Альберт','Орешек','Kedr@mail.ru')
-    #add_phone(conn,'1','89133453245')
-    #change_client(conn,1,'Fogy','Bulkin','xzxzxz@chototam.com','89831543278')
-    #delete_phone(conn,1)
-    #delete_client(conn,1)
-    find_client(conn,'Альберт')
-conn.close() 
+if __name__=="__main__":
+    with psycopg2.connect(database="test_netology", user="postgres", password="1234") as conn:
+        create_db(conn)
+        add_client(conn,'Альберт','Орешек','Kedr@mail.ru')
+        add_phone(conn,'1','89133453245')
+        change_client(conn,'1','Аристарх','Упырев')
+        print(find_client(conn,'Аристарх'))
+    conn.close() 
 
